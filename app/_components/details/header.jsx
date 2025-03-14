7ffda05a7c94daacac3e49e138888b7c3a4958b7
Birawin Jeevothayan <birawin.jeevothayan@gmail.com>
2025-03-14 03:21:56 -0400

    WIP: Initial setup with shadcn and basic header component

---
+++ app/_components/details/header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div>
+      <Image src='/logo.svg' alt='logo' 
+      width={100} height={100} />
+    </div>
+  )
+}
+
+export default header
---
+++ app/page.js
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import header from "./_components/details/header";
+
+export default function Home() {
+  return (
+    <div>
+      <h2></h2>
+
+    </div>
+  );
+}
